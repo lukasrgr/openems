7825f8a92f9d4b64f48cc62b013fc8058e18a12f
Sebastian Asen <2000asen@gmail.com>
2020-07-08 15:43:00 +0200

    Calculating the power and current values out of all three phases.

--- io.openems.edge.meter.janitza/src/io/openems/edge/meter/janitza/umg604/MeterJanitzaUmg604.java
+++ io.openems.edge.meter.janitza/src/io/openems/edge/meter/janitza/umg604/MeterJanitzaUmg604.java
@@ -10,9 +10,14 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 import org.osgi.service.metatype.annotations.Designate;
 
 import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.Unit;
+import io.openems.common.types.OpenemsType;
 import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
 import io.openems.edge.bridge.modbus.api.BridgeModbus;
 import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
@@ -22,6 +27,7 @@ import io.openems.edge.bridge.modbus.api.element.FloatDoublewordElement;
 import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
 import io.openems.edge.common.modbusslave.ModbusSlave;
 import io.openems.edge.common.modbusslave.ModbusSlaveTable;
 import io.openems.edge.common.taskmanager.Priority;
@@ -36,9 +42,12 @@ import io.openems.edge.meter.api.SymmetricMeter;
  * https://www.janitza.de/umg-604-pro.html
  */
 @Designate(ocd = Config.class, factory = true)
-@Component(name = "Meter.Janitza.UMG604", immediate = true, configurationPolicy = ConfigurationPolicy.REQUIRE)
+@Component(name = "Meter.Janitza.UMG604", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE, //
+		property = EventConstants.EVENT_TOPIC + "=" + EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE) //
 public class MeterJanitzaUmg604 extends AbstractOpenemsModbusComponent
-		implements SymmetricMeter, AsymmetricMeter, OpenemsComponent, ModbusSlave {
+		implements SymmetricMeter, AsymmetricMeter, OpenemsComponent, EventHandler, ModbusSlave {
 
 	private MeterType meterType = MeterType.PRODUCTION;
 
@@ -79,6 +88,10 @@ public class MeterJanitzaUmg604 extends AbstractOpenemsModbusComponent
 	}
 
 	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		ACTIVE_POWER_SUM(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.WATT)), //
+		CURRENT_SUM(Doc.of(OpenemsType.INTEGER) //
+				.unit(Unit.MILLIAMPERE)),
 		;
 		private final Doc doc;
 
@@ -109,12 +122,8 @@ public class MeterJanitzaUmg604 extends AbstractOpenemsModbusComponent
 						m(AsymmetricMeter.ChannelId.VOLTAGE_L3, new FloatDoublewordElement(1321), //
 								ElementToChannelConverter.SCALE_FACTOR_3)), //
 				new FC3ReadRegistersTask(1325, Priority.HIGH, //
-						m(new FloatDoublewordElement(1325)) //
-								.m(AsymmetricMeter.ChannelId.CURRENT_L1,
-										ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)) //
-								.m(SymmetricMeter.ChannelId.CURRENT,
-										ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)) //
-								.build()), //
+						m(AsymmetricMeter.ChannelId.CURRENT_L1, new FloatDoublewordElement(1325), //
+								ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert))), //
 				new FC3ReadRegistersTask(1327, Priority.HIGH, //
 						m(AsymmetricMeter.ChannelId.CURRENT_L2, new FloatDoublewordElement(1327), //
 								ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert))), //
@@ -122,12 +131,8 @@ public class MeterJanitzaUmg604 extends AbstractOpenemsModbusComponent
 						m(AsymmetricMeter.ChannelId.CURRENT_L3, new FloatDoublewordElement(1329), //
 								ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert))), //
 				new FC3ReadRegistersTask(1333, Priority.HIGH, //
-						m(new FloatDoublewordElement(1333))//
-								.m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L1,
-										ElementToChannelConverter.INVERT_IF_TRUE(this.invert))
-								.m(SymmetricMeter.ChannelId.ACTIVE_POWER,
-										ElementToChannelConverter.INVERT_IF_TRUE(this.invert))//
-								.build(), //
+						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L1, new FloatDoublewordElement(1333), //
+								ElementToChannelConverter.INVERT_IF_TRUE(this.invert)), //
 						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L2, new FloatDoublewordElement(1335), //
 								ElementToChannelConverter.INVERT_IF_TRUE(this.invert)), //
 						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L3, new FloatDoublewordElement(1337), //
@@ -143,9 +148,38 @@ public class MeterJanitzaUmg604 extends AbstractOpenemsModbusComponent
 								ElementToChannelConverter.INVERT_IF_TRUE(this.invert)), //
 						m(AsymmetricMeter.ChannelId.REACTIVE_POWER_L3, new FloatDoublewordElement(1345), //
 								ElementToChannelConverter.INVERT_IF_TRUE(this.invert))), //
+				 // Testing
+				new FC3ReadRegistersTask(1363, Priority.HIGH, //
+						m(ChannelId.CURRENT_SUM, new FloatDoublewordElement(1363), //
+								ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert))),				
+				new FC3ReadRegistersTask(1369, Priority.HIGH, //
+						m(ChannelId.ACTIVE_POWER_SUM, new FloatDoublewordElement(1369), //
+								ElementToChannelConverter.INVERT_IF_TRUE(this.invert))), //
 				new FC3ReadRegistersTask(1439, Priority.HIGH, //
 						m(SymmetricMeter.ChannelId.FREQUENCY, new FloatDoublewordElement(1439), //
 								ElementToChannelConverter.SCALE_FACTOR_3)));
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE:
+
+			int currL1 = this.getCurrentL1().orElse(0);
+			int currL2 = this.getCurrentL2().orElse(0);
+			int currL3 = this.getCurrentL3().orElse(0);
+			this._setCurrent(currL1 + currL2 + currL3);
+
+			int powerL1 = this.getActivePowerL1().orElse(0);
+			int powerL2 = this.getActivePowerL2().orElse(0);
+			int powerL3 = this.getActivePowerL3().orElse(0);
+			this._setActivePower(powerL1 + powerL2 + powerL3);
+
+			break;
+		}
 	}
 
 	@Override
