0c8d77f59bec4ec1119a8c8b53d04006f5330d82
Lukas Rieger <lukas.rieger@fenecon.de>
2023-07-06 08:41:55 +0200

    test ci build

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'cobertura', dir: 'coverage/', file: 'coverage.cobertura.xml' }
       ],
     },
     reporters: ['progress', 'kjhtml'],
